0cddc01d1f4aeea65146ec4def5e2850832967fc
Subodh Rajpopat <subodhr2001@gmail.com>
2022-12-20 16:29:45 +0530

    testing

--- client/src/AllRoutes.jsx
+++ client/src/AllRoutes.jsx
@@ -7,7 +7,7 @@ import AskQuestion from "./pages/AskQuestion/AskQuestion";
 import DisplayQuestion from "./pages/Questions/DisplayQuestion";
 import Tags from "./pages/Tags/Tags";
 import Users from "./pages/Users/Users";
-import UserProfile from './pages/UserProfile/UserProfile'
+import UserProfile from "./pages/UserProfile/UserProfile";
 
 const AllRoutes = () => {
   return (
@@ -18,8 +18,8 @@ const AllRoutes = () => {
       <Route path="/AskQuestion" element={<AskQuestion />} />
       <Route path="/Questions/:id" element={<DisplayQuestion />} />
       <Route path="/Tags" element={<Tags />} />
-      <Route path = "/Users" element = {<Users/>}/>
-      <Route path = "/Users/:id" element = {<UserProfile/>}/>
+      <Route path="/Users" element={<Users />} />
+      <Route path="/Users/:id" element={<UserProfile />} />
     </Routes>
   );
 };
--- client/src/components/Avatar/Avatar.jsx
+++ client/src/components/Avatar/Avatar.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 
-const Avatar = ({children,backgroundColor,px,py,color,
-  borderRadius,fontSize,cursor}) => {
+const Avatar = ({
+  children,
+  backgroundColor,
+  px,
+  py,
+  color,
+  borderRadius,
+  fontSize,
+  cursor,
+}) => {
   const style = {
     backgroundColor,
-    padding : `${py} ${px}`,
-    color:color || 'black',
+    padding: `${py} ${px}`,
+    color: color || "black",
     borderRadius,
     fontSize,
-    textAlign : "center",
+    textAlign: "center",
     cursor: cursor || null,
-    textDecoration : "none"
-  }
-  return(
-    <div style={style}>{children}</div>
-  )
+    textDecoration: "none",
+  };
+  return <div style={style}>{children}</div>;
 };
 
 export default Avatar;
